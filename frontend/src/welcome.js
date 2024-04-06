6e96db1fb77f4b6e654b0987ad1f032bf0793c2f
NhatHa <ha.ngnhnht@gmail.com>
2024-04-06 14:08:31 +0700

    ok color picker

---
+++ frontend/src/welcome.js
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react';
+import iro from '@jaames/iro';
+
+function WelcomePage() {
+    // useEffect(() => {
+    //     const defaultPicker = () =>
+    //         iro.ColorPicker("#defaultPicker", {
+    //             width: 250,
+    //             color: "#f00",
+    //             borderWidth: 1,
+    //             borderColor: "#fff"
+    //         });
+    //     return defaultPicker;
+    // }, []);
+
+    useEffect(() => {
+        const boxPicker = () =>
+            iro.ColorPicker("#boxPicker", {
+                width: 250,
+                color: "#f00",
+                borderWidth: 1,
+                borderColor: "#fff",
+                layout: [
+                    {
+                        component: iro.ui.Box,
+                    },
+                    {
+                        component: iro.ui.Slider,
+                        options: {
+                            id: 'hue-slider',
+                            sliderType: 'hue'
+                        }
+                    }
+                ]
+            });
+            return boxPicker;
+    }, []);
+
+    return (
+        <div>
+            <div className="Grid">
+                <div className="ColorPicker" id="boxPicker">
+                    <h3>hehe</h3>
+                    <boxPicker />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default WelcomePage
